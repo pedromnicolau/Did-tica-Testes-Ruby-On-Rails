c442a391e2962382297928655a87be40e6ddce69
Pedro Nicolau <pnicolau@eslsistemas.com.br>
2025-11-04 16:52:13 -0300

    Testes de regras básicas dos outros models

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Deal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:property) }
+  it { should belong_to(:lead) }
+  it { should validate_numericality_of(:price_cents).is_greater_than(0) }
 end
--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Lead, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:property).optional }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
 end
